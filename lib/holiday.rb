a0e95c92b6271561907acad6efa4a9d142b9f6bd
Toni  Diep <adiep123@gmail.com>
2020-05-08 19:33:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  #{event.to_s.split("_").map { |x| x.capitalize }.join(" ") }: #{supplies.join(", ")}"
     end
   end
-  binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
